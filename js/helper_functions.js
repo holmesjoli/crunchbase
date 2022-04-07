9bd9b63afdb15be6d0c96cc7eb475b0d5e606d07
Joli Holmes <holmesjoli@gmail.com>
2022-04-07 19:38:38 -0400

    simplified grid

--- js/helper_functions.js
+++ js/helper_functions.js
@@ -1,59 +1,22 @@
-// Title xPosition
-// Description creates an array which can be used to set x
-function xPosition(nCol, nRow, xSpace, xStart) {
-    const nArray = [];
-    for (var i = 1; i <= nCol; i++) {
-        nArray.push(i*xSpace + xStart - xSpace);
+// Title Create Grid
+function grid(params, top) {
+
+    const colW = params.w / params.cols;
+    const rows = Math.round(params.n/params.cols)
+    const rowH = params.h / rows;
+
+    for (let i = 0; i < params.n; i++) {
+
+        let col = i % params.cols;
+        let row = Math.floor(i / params.cols);
+
+        top[i].radius = sqrt(top[i].total / 1E6)/4;
+        top[i].x = params.startPos.x + top[i].radius + (colW * col)
+        top[i].y = params.startPos.y + top[i].radius + (rowH * row)
+        console.log(top[i])
     }
 
-    return [].concat.apply([], Array(nRow).fill(nArray));
-}
-
-// Title yPosition
-// Description creates an array which can be used to set y
-function yPosition(nCol, nRow, ySpace, yStart) {
-    var foo = [];
-        for (var i = 1; i <= nRow; i++) {
-            for (var j = 1; j <= nCol; j++) {
-            foo.push(i*ySpace + yStart);
-        }
-    }
-
-    return foo;
-}
-
-// Position on a grid
-function position(data, nCol, nRow, xSpace = 100, ySpace = 100, xStart = 0, yStart = 0) {
-
-    const xPos = xPosition(nCol, nRow, xSpace, xStart);
-    const yPos = yPosition(nCol, nRow, ySpace, yStart);
-
-    data.forEach(function (d, i) {
-        d.x = xPos[i];
-        d.y = yPos[i];
-        d.xSpace = xSpace;
-        d.ySpace = ySpace;
-    });
-
-    return data;
-}
-
-// Title Generate Matrix
-// Description Geneates the number of columns and number of rows
-// Which depend on the number of countries
-function generateMatrix(nObj) {
-    let nRow;
-
-    if (nObj >= 14) {
-        nRow = 2;
-    } else {
-        nRow = 1;
-    }
-
-    return {
-        nCol: Math.ceil(nCntry / nRow),
-        nRow: nRow,
-    };
+    return top;
 }
 
 // Number formatter
